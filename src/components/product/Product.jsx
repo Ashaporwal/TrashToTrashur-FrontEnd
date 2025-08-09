413fc713241111f1b382aa55d737062145a6d45a
ashap127 <porwalasha58@gmail.com>
2025-08-09 09:49:27 +0530

    error fix

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../Header";
 import Footer from "../Footer";
-import productData from "../data/productData";
 import { useNavigate } from "react-router-dom";
 
 function Product() {
