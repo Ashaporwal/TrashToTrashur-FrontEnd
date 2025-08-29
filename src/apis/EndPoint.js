d22db08c3c72a4435e233aba6ddd54a9ed300f30
ashap127 <porwalasha58@gmail.com>
2025-08-29 22:40:54 +0530

    error fix

--- src/apis/EndPoint.js
+++ src/apis/EndPoint.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000";
-// const BASE_URL = "https://your-backend-render-url.onrender.com";
+// export const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://your-backend-render-url.onrender.com";
 
 export default {
   SIGN_UP: BASE_URL + "/user/signup",
